1e1f4b44ebcd16c72e21215c17eb2f2178bbb5a2
Hemavathi.M <hemavathimreddy1@gmail.com>
2026-04-22 16:02:02 +0530

    Initial Commit

---
+++ QRProductScanner/QRProductScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRProductScannerApp.swift
+//  QRProductScanner
+//
+//  Created by Hemavathi on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct QRProductScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
